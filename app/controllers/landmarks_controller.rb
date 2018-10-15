a430b704e43496512ba7ae68f6d38988e23243a9
Patricia Nykaza <pnykaza@calstatela.edu>
2018-10-15 19:01:25 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -8,26 +8,25 @@ class LandmarksController < ApplicationController
     erb :"landmarks/new"
   end
 
-  post '/landmarks/' do
-    @landmark = Landmark.create(params["landmark"])
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
     redirect to "/landmarks/#{@landmark.id}"
   end
 
   get '/landmarks/:id/edit' do
-    @landmark = Landmark.find_by(:id => params["id"])
+    @landmark = Landmark.find(params[:id])
     erb :"landmarks/edit"
   end
 
-  post '/landmarks/:id' do
-    @landmark = Landmark.find_by(:id => params["id"])
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
     @landmark.update(params[:landmark])
     @landmark.save
     redirect to "/landmarks/#{@landmark.id}"
   end
 
   get '/landmarks/:id' do
-    @landmark = Landmark.find_by(:id => params["id"])
+    @landmark = Landmark.find(params[:id])
     erb :"landmarks/show"
   end
-
 end
