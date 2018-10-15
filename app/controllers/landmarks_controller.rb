b5d38a5e08b02ae2975718dbc1680b3187816905
Patricia Nykaza <pnykaza@calstatela.edu>
2018-10-15 18:59:29 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,11 +1,10 @@
 class LandmarksController < ApplicationController
   get '/landmarks' do
     @landmarks = Landmark.all
-    erb :"/landmarks/index"
+    erb :"landmarks/index"
   end
 
   get '/landmarks/new' do
-    @landmarks = Landmark.all
     erb :"landmarks/new"
   end
 
